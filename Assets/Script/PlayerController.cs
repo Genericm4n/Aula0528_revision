aa2bbc3d0c2a8470153f9b8e81252626e226d074
Júlio "genericman" César <47559029+Genericm4n@users.noreply.github.com>
2019-05-28 08:56:21 -0300

    Initial commit

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Animator anime;
+
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+        var h = Input.GetAxisRaw("Horizontal");
+        var v = Input.GetAxisRaw("Vertical");
+
+        var vel = 2f;
+        var velRot = 200f;
+
+        transform.Translate(Vector3.forward * v * vel * Time.deltaTime);
+        transform.Rotate(Vector3.up * h * velRot * Time.deltaTime);
+
+        anime.SetFloat("speed", v);
+    }
+}
