db10137a66511a53a2047fb9fde31bbc148653ab
Júlio "genericman" César <47559029+Genericm4n@users.noreply.github.com>
2019-05-28 11:11:09 -0300

    Basico de Blendtree, Follow Target e LookAt

---
+++ Assets/Script/CubeController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+    public GameObject objectLookAt;
+
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+        transform.LookAt(objectLookAt.transform);
+
+        var velT = 2f;
+
+        var direction = (objectLookAt.transform.position - transform.position).normalized;
+
+        transform.Translate(Vector3.forward * velT * Time.deltaTime);
+    }
+}
